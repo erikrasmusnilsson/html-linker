14d108f8acf79dbde604443bad4dd7a4fa348325
Rasmus Nilsson <info@rasmusnilsson.se>
2018-12-29 14:02:01 +0100

    Added support for scss s files

--- linker.py
+++ linker.py
@@ -1,14 +1,19 @@
-from os import listdir 
+from os import listdir
+from os import system
 import sys
 
+sass = False
+
 # Validate parameters
-if len(sys.argv) > 3:
+if len(sys.argv) < 3:
     print "Feed me more parameters..."
     sys.exit()
 elif sys.argv[1][-3:len(sys.argv[1])] != "pth":
     print "I only accept .pth files to convert."
 elif sys.argv[2][-4:len(sys.argv[2])] != "html":
     print "I can only generate .html files!"
+elif len(sys.argv) > 3 and sys.argv[3] == "--sass":
+    sass = True
 
 # Open/create files
 input = open(sys.argv[1], "r")
@@ -24,10 +29,16 @@ while i < len(inputmod):
         inputmod.remove(inputmod[i])
         j = i
         difference = j + len(filenames)
+        cursor = j
         while j < difference:
             index = j - i
-            if ".css" in filenames[index]:
-                inputmod.insert(j, "\t\t<link rel='stylesheet' type='text/css' href='css/"+filenames[index]+"'>")
+            if sass:
+                sassfiles = listdir("sass/")
+                for file in sassfiles:
+                    filename = file[0:file.find(".")]
+                    system("sass sass/%s css/%s" % (filename + ".scss", filename + ".css"))
+            if filenames[index][-4:len(filenames[index])] == ".css":
+                inputmod.insert(cursor, "\t\t<link rel='stylesheet' type='text/css' href='css/"+filenames[index]+"'>")
             j += 1
     elif "<javascript/>" in inputmod[i]:
         filenames = listdir("javascript/")
@@ -46,3 +57,7 @@ output.write("\n".join(inputmod))
 # Make sure you close files after use.
 input.close()
 output.close()
+
+# If sass option is true, watch the sass files
+if sass:
+    system("sass --watch sass:css")
